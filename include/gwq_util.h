c7f4b99a52988cc30ab3249cd7f40995f980e0b7
CrazyPandar <CrazyPandar@gmail.com>
2012-07-07 21:40:19 +0800

    fix a bug: g_print can not print utf-8 chinese characters correctly.

--- include/gwq_util.h
+++ include/gwq_util.h
@@ -3,19 +3,20 @@
 #include <string.h>
 #include <sqlite3.h>
 #include <glib.h>
+#include <glib/gprintf.h>
 #include <libsoup/soup.h>
 #include <json-glib/json-glib.h>
 
 #define _DEBUG_
 #ifdef _DEBUG_
-#define GWQ_DBG(...) {g_print("[D][%s:%d]", __FILE__, __LINE__);g_print(__VA_ARGS__);}
+#define GWQ_DBG(...) {g_printf("[D][%s:%d]", __FILE__, __LINE__);g_printf(__VA_ARGS__);}
 #else
 #define GWQ_DBG(...)
 #endif
-#define GWQ_MSG(...)	{g_print("[M][%s:%d]", __FILE__, __LINE__);g_print(__VA_ARGS__);}
-#define GWQ_WARN(...)	{g_print("[W][%s:%d]", __FILE__, __LINE__);g_print(__VA_ARGS__);}
-#define GWQ_ERR(...) {g_print("[E][%s:%d]", __FILE__, __LINE__);g_print(__VA_ARGS__);}
-#define GWQ_ERR_OUT(__label, ...)	{g_print("[E][%s:%d]", __FILE__, __LINE__);g_print(__VA_ARGS__);goto __label;}
+#define GWQ_MSG(...)	{g_printf("[M][%s:%d]", __FILE__, __LINE__);g_printf(__VA_ARGS__);}
+#define GWQ_WARN(...)	{g_printf("[W][%s:%d]", __FILE__, __LINE__);g_printf(__VA_ARGS__);}
+#define GWQ_ERR(...) {g_printf("[E][%s:%d]", __FILE__, __LINE__);g_printf(__VA_ARGS__);}
+#define GWQ_ERR_OUT(__label, ...)	{g_printf("[E][%s:%d]", __FILE__, __LINE__);g_printf(__VA_ARGS__);goto __label;}
 
 
 glong GetNowMillisecond();
